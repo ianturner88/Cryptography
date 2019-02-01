c91e6851f77f80a42aee66bf1aa066f643ae95c5
Ian Turner <ianturner88@gmail.com>
2019-02-01 13:51:18 -0400

    length function built

--- HM1_AC/HM1_AC/HM1_AC.cpp
+++ HM1_AC/HM1_AC/HM1_AC.cpp
@@ -4,11 +4,13 @@
 #include<streambuf>
 using namespace std;
 
+void Input_String_Length(string input_string, int &input_string_length);
+
 int main(void)
 {
 	ifstream fin("HM1_AC.txt");
 
-	int size;
+	int input_string_length = 0;
 
 	//read text file into a string
 	string input_string((istreambuf_iterator<char>(fin)), istreambuf_iterator<char>());
@@ -16,4 +18,11 @@ int main(void)
 	cout << input_string << endl;
 
 	getchar();
+}
+
+void Input_String_Length(string input_string, int &input_string_length)
+{
+	/*determines the length of the input string*/
+	
+	input_string_length = input_string.length();
 }
