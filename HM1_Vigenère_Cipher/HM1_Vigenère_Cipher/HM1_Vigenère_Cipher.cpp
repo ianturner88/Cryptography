feee4111bf9c30002e75f79436d85aec44c48b6c
Ian Turner <ianturner88@gmail.com>
2019-02-04 11:25:42 -0400

    the march continues

--- HM1_Vigenère_Cipher/HM1_Vigenère_Cipher/HM1_Vigenère_Cipher.cpp
+++ HM1_Vigenère_Cipher/HM1_Vigenère_Cipher/HM1_Vigenère_Cipher.cpp
@@ -2,22 +2,30 @@
 #include<string>
 #include<fstream>
 #include<streambuf>
-#include <iomanip>
+#include<iomanip>
+#include<vector>
 using namespace std;
 
 void Input_String_Length(string input_string, int &input_string_length);
 void Alphabet(string letters[]);
 void Comparison_Test(int input_string_length, string input_string, int comparison_counter, string letters[]);
+void Letter_Frequency(int letter_frequency[]);
+void User_Inputs_Key_Length(int &key_length);
+void Letter_Count_at_Interval_X(int letter_count[], string input_string, int key_length, string letters[],
+	int number_of_letters);
 
 int main(void)
 {
 	ifstream fin("Vigenère.txt");
 	string input_string((istreambuf_iterator<char>(fin)), istreambuf_iterator<char>());
-	int input_string_length = 0, letter_count[26] = { 0 }, comparison_counter = 0;
-	string letters[26];
+	int input_string_length = 0, letter_count[26] = { 0 }, comparison_counter = 0, letter_frequency[26],
+		key_length = 0, number_of_letters = 0, for_loop_counter = 0;
+	string letters[26], letter_summation = "";
+	vector<int> letter_frequencies;
 
-	//initialize the alphabet
+	//initialize the hard-coded arrays
 	Alphabet(letters);
+	Letter_Frequency(letter_frequency);
 
 	//calculate the length of the string
 	Input_String_Length(input_string, input_string_length);
@@ -25,7 +33,47 @@ int main(void)
 	//identifies matches in the shifted string vs. base string
 	Comparison_Test(input_string_length, input_string, comparison_counter, letters);
 
+	User_Inputs_Key_Length(key_length);
+
+	for (for_loop_counter; for_loop_counter < key_length; for_loop_counter++)
+	{
+
+	}
+
 	getchar();
+}
+
+void Letter_Count_at_Interval_X(int letter_count[], string input_string, int key_length, string letters[], 
+	int number_of_letters, int &for_loop_counter)
+{
+	string individual_letter;
+	bool match = false;
+	
+	int j = for_loop_counter;
+
+	while (j < input_string.length())
+	{
+		individual_letter = input_string[j];
+		number_of_letters++;
+			
+		for (int k = 0; (k < 26) && (match == false); k++)
+		{
+			if (individual_letter == letters[k])
+			{
+				letter_count[k] += 1;
+				match = true;
+			}
+		}
+			
+		match = false;
+		j += key_length;
+	}
+}
+
+void User_Inputs_Key_Length(int &key_length)
+{
+	cout << "Please enter the key length: ";
+	cin >> key_length;
 }
 
 void Input_String_Length(string input_string, int &input_string_length)
@@ -98,4 +146,34 @@ void Alphabet(string letters[])
 	letters[23] = "x";
 	letters[24] = "y";
 	letters[25] = "z";
+}
+
+void Letter_Frequency(int letter_frequency[])
+{
+	letter_frequency[0] = 8.167;
+	letter_frequency[1] = 1.492;
+	letter_frequency[2] = 2.782;
+	letter_frequency[3] = 4.253;
+	letter_frequency[4] = 12.702;
+	letter_frequency[5] = 2.228;
+	letter_frequency[6] = 2.015;
+	letter_frequency[7] = 6.094;
+	letter_frequency[8] = 6.966;
+	letter_frequency[9] = 0.153;
+	letter_frequency[10] = 0.772;
+	letter_frequency[11] = 4.025;
+	letter_frequency[12] = 2.406;
+	letter_frequency[13] = 6.749;
+	letter_frequency[14] = 7.507;
+	letter_frequency[15] = 1.929;
+	letter_frequency[16] = 0.095;
+	letter_frequency[17] = 5.987;
+	letter_frequency[18] = 6.327;
+	letter_frequency[19] = 9.056;
+	letter_frequency[20] = 2.758;
+	letter_frequency[21] = 0.978;
+	letter_frequency[22] = 2.360;
+	letter_frequency[23] = 0.150;
+	letter_frequency[24] = 1.974;
+	letter_frequency[25] = 0.074;
 }
